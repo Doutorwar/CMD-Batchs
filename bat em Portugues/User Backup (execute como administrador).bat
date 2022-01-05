ca62a9593890e6bdfc0c380bc038a04d00716009
Felipe P <felipepinheirocarneiro29@gmail.com>
2022-01-05 11:21:48 -0300

    Update User Backup (execute como administrador).bat

--- bat em Portugues/User Backup (execute como administrador).bat
+++ bat em Portugues/User Backup (execute como administrador).bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-title User Backup by Doutorwar
+title User Backup por Doutorwar
 cls
 
 echo Em qual disco esta o user que sera copiado? (EX: Disco C) Digite apenas a letra
@@ -8,6 +8,7 @@ cls
 
 echo Para qual disco sera copiado o arquivo? (EX: Disco D) Digite apenas a letra
 set/p "diretoriof=>"
+
 cd %diretorio%:\Users\
 cls
 echo Diretorios:
@@ -16,9 +17,11 @@ echo.
 echo Qual o user acima que sera copiado?
 set/p "user=>"
 cls
+
 @ECHO ON
 xcopy "%diretorio%:\Users\%user%" "%diretoriof%:\backup %user% disco %diretorio%" /e /h /c
 cls
+
 @ECHO OFF
 echo Backup concluido
 pause
