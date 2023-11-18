d00751891e22ea8ddec73ad557c37ec3b9d26010
Felipe Pinheiro <felipepinheirocarneiro29@gmail.com>
2023-11-18 11:24:36 -0300

    Update BatTools.bat
    update the link for github

--- BatTools.bat
+++ BatTools.bat
@@ -3,7 +3,7 @@ ECHO OFF
 cls
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: meu github https://github.com/Doutorwar                                  ::
+:: meu github https://github.com/felpsfpp                                   ::
 :: Programa criado por Doctorwar, feito para ajudar tecnicos de informatica ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
