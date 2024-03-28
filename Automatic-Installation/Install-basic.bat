c47dd5d47c35d911292be403a644370086d80d0e
Felipe Pinheiro <felipepinheirocarneiro29@gmail.com>
2024-03-27 22:08:04 -0300

    Add Install-basic.bat

---
+++ Automatic-Installation/Install-basic.bat
@@ -0,0 +1,24 @@
+winget install -e --id Microsoft.VCRedist.2005.x86
+winget install -e --id Microsoft.VCRedist.2005.x64
+
+winget install -e --id Microsoft.VCRedist.2008.x64
+winget install -e --id Microsoft.VCRedist.2008.x86
+
+winget install -e --id Microsoft.VCRedist.2010.x64
+winget install -e --id Microsoft.VCRedist.2010.x86
+
+winget install -e --id Microsoft.VCRedist.2012.x64
+winget install -e --id Microsoft.VCRedist.2012.x86
+
+winget install -e --id Microsoft.VCRedist.2013.x64
+winget install -e --id Microsoft.VCRedist.2013.x86
+
+winget install -e --id Microsoft.VCRedist.2015+.x64
+winget install -e --id Microsoft.VCRedist.2015+.x86
+
+
+winget install -e --id Oracle.JavaRuntimeEnvironment
+winget install -e --id 7zip.7zip
+winget install -e --id VideoLAN.VLC
+winget install -e --id dotPDNLLC.paintdotnet
+exit
